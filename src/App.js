bb9547ec5fe55fcd7d8fe1be923ef06521785b6a
Ayush Mukherjee <ayushm@drreddys.com>
2025-03-18 00:44:13 +0100

    minor changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Made some new changes here
+          Made some more new changes here
         </p>
         <a
           className="App-link"
