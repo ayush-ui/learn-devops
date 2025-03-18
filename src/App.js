a5da62c55bb1a85edae027a88bd1fe9f880e480d
Ayush Mukherjee <ayushm@drreddys.com>
2025-03-18 01:11:22 +0100

    changes in deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Made some more new changes here
+          Made some more changes here
         </p>
         <a
           className="App-link"
